a07158c9b85f42422c8c586d80e6c0ae1001a948
Alan D. Cabrera <adc@toolazydogs.com>
2014-02-16 13:17:56 -0800

    setup cleanup

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-"""
- Copyright 2014 the original author or authors
+#
+# Copyright 2014 the original author or authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
-"""
+from io import open
+import subprocess
+import sys
 
 from setuptools import find_packages, setup
-from codecs import open
 
 from shrew import __version__
 
@@ -36,14 +39,18 @@ setup(
     description='A handy way to manage your bash environment across machines.',
     # don't ever depend on refcounting to close files anywhere else
     long_description=open('README.rst', encoding='utf-8').read(),
-    packages=find_packages(exclude=['tests']),
+
+    packages=['shrew'],
     scripts=['bin/shrew'],
     zip_safe=False,
     platforms='any',
-    test_suite='shrew',
     install_requires=install_requires,
+
+    test_suite='shrew',
     tests_require=tests_require,
+
     classifiers=[
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
